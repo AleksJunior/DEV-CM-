af5ceb6b226ab106b0d340087ad487aacea0500a
Alex Bird <98682869+AleksJunior@users.noreply.github.com>
2026-03-31 11:19:39 +0300

    Update-CertificatesV2_Core.ps1

--- Update-CertificatesV2_Core.ps1
+++ Update-CertificatesV2_Core.ps1
@@ -1,4 +1,7 @@
 ﻿<#
+Copyright (c) 2026 Alex Bird
+Use of this source code is governed by an MIT-style
+license that can be found in the LICENSE file.
 .SYNOPSIS
     Ядро обновления сертификатов и списков отзыва (CRL)
 .DESCRIPTION
