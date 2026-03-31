bdf2734234d80933ba471a1571eb4a3258827d62
Alex Bird <98682869+AleksJunior@users.noreply.github.com>
2026-03-31 11:21:05 +0300

    Create-ScheduledTaskV2.ps1

--- Create-ScheduledTaskV2.ps1
+++ Create-ScheduledTaskV2.ps1
@@ -1,4 +1,7 @@
 ﻿<#
+Copyright (c) 2026 Alex Bird
+Use of this source code is governed by an MIT-style
+license that can be found in the LICENSE file.
 .SYNOPSIS
     Создание задачи в планировщике Windows для автоматического обновления сертификатов
 .DESCRIPTION
