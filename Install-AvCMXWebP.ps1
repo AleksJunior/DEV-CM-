41aaa1ebcae75af2876b510f5b738b782db429ed
Alex Bird <98682869+AleksJunior@users.noreply.github.com>
2026-03-31 11:20:29 +0300

    Install-AvCMXWebP.ps1

--- Install-AvCMXWebP.ps1
+++ Install-AvCMXWebP.ps1
@@ -1,4 +1,7 @@
 ﻿<#
+Copyright (c) 2026 Alex Bird
+Use of this source code is governed by an MIT-style
+license that can be found in the LICENSE file.
 .SYNOPSIS
     Установка плагина AvCMXWebP для работы с порталами в режиме IE Mode
 .DESCRIPTION
