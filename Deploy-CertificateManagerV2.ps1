b469837e46d95d6290d3329da04f2ff604955099
Alex Bird <98682869+AleksJunior@users.noreply.github.com>
2026-03-31 11:20:46 +0300

    Update Deploy-CertificateManagerV2.ps1

--- Deploy-CertificateManagerV2.ps1
+++ Deploy-CertificateManagerV2.ps1
@@ -1,4 +1,7 @@
 ﻿<#
+Copyright (c) 2026 Alex Bird
+Use of this source code is governed by an MIT-style
+license that can be found in the LICENSE file.
 .SYNOPSIS
     Развертывание Менеджера сертификатов V2.0 в целевой папке
 .DESCRIPTION
